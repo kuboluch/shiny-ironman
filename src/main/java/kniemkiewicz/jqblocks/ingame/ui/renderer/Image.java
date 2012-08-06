42728b60b5e5add407514db76761169761c097cf
kuboluch <kuboluch@gmail.com>
2012-08-06 23:49:15 +0200

    Suppress idea warning

--- src/main/java/kniemkiewicz/jqblocks/ingame/ui/renderer/Image.java
+++ src/main/java/kniemkiewicz/jqblocks/ingame/ui/renderer/Image.java
@@ -14,6 +14,7 @@ import javax.annotation.PostConstruct;
  */
 public class Image implements de.matthiasmann.twl.renderer.Image {
 
+  @SuppressWarnings({"SpringJavaAutowiringInspection"})
   @Autowired
   GUI gui;
 
