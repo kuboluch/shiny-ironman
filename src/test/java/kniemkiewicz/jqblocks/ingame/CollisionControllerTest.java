dbeb3fb96cdfbddd35d3bc34f4fe0c208927a90e
kuboluch <kuboluch@gmail.com>
2012-08-11 22:41:32 +0200

    Finding place for newly mined resource object fixed

---
+++ src/test/java/kniemkiewicz/jqblocks/ingame/CollisionControllerTest.java
@@ -0,0 +1,37 @@
+package kniemkiewicz.jqblocks.ingame;
+
+import kniemkiewicz.jqblocks.ingame.resource.ResourceObject;
+import kniemkiewicz.jqblocks.ingame.resource.Wood;
+import org.junit.Assert;
+import org.junit.Test;
+import org.newdawn.slick.geom.Rectangle;
+
+import java.util.EnumSet;
+
+/**
+ * User: qba
+ * Date: 11.08.12
+ */
+public class CollisionControllerTest {
+  private static final EnumSet<CollisionController.ObjectType> PICKABLE =
+      EnumSet.of(CollisionController.ObjectType.PICKABLE);
+
+  @Test
+  public void doTest() {
+    CollisionController collisionController = new CollisionController();
+
+    ResourceObject<Wood> woodObject = new ResourceObject<Wood>(new Wood(), 0, 100);
+    collisionController.add(PICKABLE, woodObject, false);
+
+    Rectangle test = new Rectangle(0, 100, 1, 1);
+    Assert.assertTrue(collisionController.intersects(PICKABLE, test));
+    Assert.assertNotNull(collisionController.fullSearch(PICKABLE, test));
+    Assert.assertTrue(collisionController.fullSearch(PICKABLE, test).size() == 1);
+
+    test.setX(ResourceObject.SIZE);
+    // TODO should it intersect or not?
+    Assert.assertFalse(collisionController.intersects(PICKABLE, test));
+    Assert.assertNotNull(collisionController.fullSearch(PICKABLE, test));
+    Assert.assertTrue(collisionController.fullSearch(PICKABLE, test).isEmpty());
+  }
+}
