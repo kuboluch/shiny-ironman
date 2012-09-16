4b83808557e013f465fdc4a7e92e3a72cd53ff8d
kniemkiewicz <user@email>
2012-09-16 02:14:03 +0200

    mirroring stuckarrow when target switches direction

--- src/main/java/kniemkiewicz/jqblocks/ingame/content/item/arrow/StuckArrow.java
+++ src/main/java/kniemkiewicz/jqblocks/ingame/content/item/arrow/StuckArrow.java
@@ -1,9 +1,6 @@
 package kniemkiewicz.jqblocks.ingame.content.item.arrow;
 
-import kniemkiewicz.jqblocks.ingame.PointOfView;
-import kniemkiewicz.jqblocks.ingame.RenderQueue;
-import kniemkiewicz.jqblocks.ingame.Sizes;
-import kniemkiewicz.jqblocks.ingame.UpdateQueue;
+import kniemkiewicz.jqblocks.ingame.*;
 import kniemkiewicz.jqblocks.ingame.content.hp.KillablePhysicalObject;
 import kniemkiewicz.jqblocks.ingame.content.item.bow.BowRenderer;
 import kniemkiewicz.jqblocks.ingame.object.HasSource;
@@ -37,11 +34,15 @@ public class StuckArrow implements RenderableObject<StuckArrow>,UpdateQueue.ToBe
   transient Line line;
   transient Vector2f diff;
   final SerializableRef<KillablePhysicalObject> target;
+  Boolean lastDirection = null;
 
   public StuckArrow(Line line, KillablePhysicalObject target) {
     this.line = line;
     this.target = new SerializableRef<KillablePhysicalObject>(target);
     this.diff = new Vector2f(line.getCenterX() - target.getShape().getCenterX(), line.getCenterY() - target.getShape().getCenterY());
+    if (target instanceof HasFullXYMovement) {
+      this.lastDirection = ((HasFullXYMovement) target).getXYMovement().getXMovement().getDirection();
+    }
   }
 
   public void addTo(RenderQueue renderQueue, UpdateQueue updateQueue) {
@@ -71,6 +72,14 @@ public class StuckArrow implements RenderableObject<StuckArrow>,UpdateQueue.ToBe
 
 
   public void update() {
+    if (lastDirection != null) {
+      if (this.lastDirection != ((HasFullXYMovement) target.get()).getXYMovement().getXMovement().getDirection()) {
+        this.lastDirection = ((HasFullXYMovement) target.get()).getXYMovement().getXMovement().getDirection();
+        // flip line horizontally
+        line.set(line.getX2(), line.getY1(), line.getX1(), line.getY2());
+        diff.set(- diff.getX(), diff.getY());
+      }
+    }
     line.setCenterX(this.target.get().getShape().getCenterX() + diff.getX());
     line.setCenterY(this.target.get().getShape().getCenterY() + diff.getY());
   }
